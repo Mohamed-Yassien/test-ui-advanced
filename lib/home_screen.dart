e20a39237498d453a0ed4b7454e171f22b83acc6
MAC ITADVIP <macitadvip@192.168.1.5>
2023-12-17 03:26:01 -0800

    show date in screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int numberOfDays = 0;
   int currentIndex = 0;
+
+  DateTime? selectedDate;
 
   @override
   void initState() {
@@ -39,11 +42,21 @@ class _HomeScreenState extends State<HomeScreen> {
       31
     ];
     numberOfDays = daysInMonth[month - 1];
+    selectedDate = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      currentIndex + 1,
+    );
     return daysInMonth[month - 1];
   }
 
   changeCurrentIndex({required int index}) {
     currentIndex = index;
+    selectedDate = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      currentIndex + 1,
+    );
     debugPrint(
         "this date is ${DateTime(DateTime.now().year, DateTime.now().month, currentIndex + 1)}");
     setState(() {});
@@ -52,54 +65,66 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("date time"),
-      ),
-      body: Column(children: [
-        SizedBox(
-          height: 80,
-          child: ListView.separated(
-            padding: const EdgeInsets.all(5),
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  changeCurrentIndex(index: index);
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: currentIndex == index ? Colors.teal : Colors.white,
-                    border: Border.all(
-                      color: currentIndex == index
-                          ? Colors.grey.withOpacity(.4)
-                          : Colors.grey.withOpacity(.4),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 60),
+          SizedBox(
+            height: 80,
+            child: ListView.separated(
+              padding: const EdgeInsets.all(5),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    changeCurrentIndex(index: index);
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: currentIndex == index ? Colors.teal : Colors.white,
+                      border: Border.all(
+                        color: currentIndex == index
+                            ? Colors.grey.withOpacity(.4)
+                            : Colors.grey.withOpacity(.4),
+                      ),
+                    ),
+                    child: Text(
+                      "${index + 1}",
+                      style: TextStyle(
+                        color:
+                            currentIndex == index ? Colors.white : Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "${index + 1}",
-                    style: TextStyle(
-                      color:
-                          currentIndex == index ? Colors.white : Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(
-                width: 15,
-              );
-            },
-            itemCount: numberOfDays,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  width: 15,
+                );
+              },
+              itemCount: numberOfDays,
+            ),
           ),
-        ),
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              DateFormat('yyyy-MM-dd').format(selectedDate!),
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
